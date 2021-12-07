16c725837fa5dd6b389c991077b344bfe234aced
kyki73394 <kyki73394@gmail.com>
2021-12-07 09:57:54 -0500

    attempting nav

---
+++ react_packages/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Logo from "../assets/curis_loogo.png"
+
+function Navbar() {
+  return (
+    <divc className = "Navbar">
+      <div className = "leftSide">
+        <img src = {Logo}>
+      </div>
+      <div className = "rightSide"></div>
+    </div>
+  )
+}
