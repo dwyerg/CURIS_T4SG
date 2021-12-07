50e6c320a32273bd3b493feef02933600ac6d9fb
kyki73394 <kyki73394@gmail.com>
2021-12-07 15:58:17 -0500

    starting nav over

--- react_packages/src/App.js
+++ react_packages/src/App.js
@@ -1,18 +1,13 @@
+// import React from "react";
 import './App.css';
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        // <Switch>
-        //   <Route path = "/" exact component = {Home} />
-        // </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
