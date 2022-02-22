d905672ab50c4e9591a41f8a896a319680e32dc9
alexbnewman <alex02new@outlook.com>
2022-02-22 01:45:39 -0500

    updates to learnMore

--- react_packages/src/pages/learnMore.js
+++ react_packages/src/pages/learnMore.js
@@ -1,5 +1,6 @@
 //import './App.css';
 import './learnMore.css';
+import claire from '../assets/claire.PNG';
 
 const LearnMore = () => {
   return (
@@ -10,21 +11,28 @@ const LearnMore = () => {
         <div className="row">
           <div className="column" style={{ backgroundColor: '#88b3da'}}>
             <h2 id = "w">Consult</h2>
-            <p id = "w">We <b>aid local organizations</b> in their efforts to improve public health</p>
+            <p id = "w" style={{ textAlign: "center"}}>We <b>aid local organizations</b> in their efforts to improve public health</p>
           </div>
           <div className="column" style={{ backgroundColor: '#e6f2f6'}}>
             <h2 id="b2">Educate</h2>
-            <p id="b2">We <b>raise awareness</b> of various public health issues relevant to the local area</p>
+            <p id="b2" style={{ textAlign: "center"}}>We <b>raise awareness</b> of various public health issues relevant to the local area</p>
           </div>
           <div className="column" style={{ backgroundColor: '#88b3da'}}>
-            <h2 id = "w">Consult</h2>
-            <p id = "w">We strive to continuously <b>advance our understanding</b> of public health issues</p>
+            <h2 id = "w">Learn</h2>
+            <p id = "w" style={{ textAlign: "center"}}>We strive to continuously <b>advance our understanding</b> of public health issues</p>
           </div>
+        </div>
+        <h1>Interested in Becoming a Member?</h1>
+        <h4>Here are some responsibilities of membership:</h4>
+        <div className="rectangle" id="r1">
+          <img src={claire} alt="claire" style={{float: "left"}}/>
+          <h3>Attendance</h3>
+          <p>At <b>general body meetings, committee meetings</b> (every other week respectively) and our <b>social events</b></p>
+
         </div>
 
 
       </centerAllCont>
-      <h3>2020-2021</h3>
 
     </>
   );
