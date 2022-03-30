cff4edd9d01c54fe955be2b6f7231066856abe01
kyki73394 <kyki73394@gmail.com>
2022-03-29 23:31:59 -0400

    resized images (members pg)

--- react_packages/src/pages/Members.js
+++ react_packages/src/pages/Members.js
@@ -38,7 +38,7 @@ const Members = ({children, src, alt, Wrapper = 'div'}) => {
 
         <div className="grid">
 
-          <div className="imgAndName" onClick={toggleIsOpen} style={{ cursor: 'pointer' }} >
+          <div className="imgAndName2" onClick={toggleIsOpen} style={{ cursor: 'pointer' }} >
           <Wrapper>
               {children}
               {isOpen ?
@@ -48,7 +48,7 @@ const Members = ({children, src, alt, Wrapper = 'div'}) => {
                   <div className="blueBg"></div>
                   <div className="innerBg"></div>
 
-                  <img src={marilyn} alt="Marilyn Li, the President of CURIS" className="bigImg"/>
+                  <div className = "imgCont2"><img src={marilyn} alt="Marilyn Li, the President of CURIS" className="bigImg"/></div>
                   <div className="bigName">
                     <h4>Marilyn Li</h4>
                     <h5>President</h5>
@@ -63,12 +63,12 @@ const Members = ({children, src, alt, Wrapper = 'div'}) => {
                 : null}
             </Wrapper>
 
-            <img src={marilyn} alt="Marilyn Li, the President of CURIS" className="smallImg" />
+            <div className = "imgCont"><img src={marilyn} width="250px" alt="Marilyn Li, the President of CURIS" className="smallImg" /></div>
             <h4>Marilyn Li</h4>
             <h5>President</h5>
           </div>
 
-          <div className="imgAndName" onClick={toggleIsOpen2} style={{ cursor: 'pointer' }} >
+          <div className="imgAndName2" onClick={toggleIsOpen2} style={{ cursor: 'pointer' }} >
           <Wrapper>
               {children}
               {isOpen2 ?
@@ -93,28 +93,28 @@ const Members = ({children, src, alt, Wrapper = 'div'}) => {
                 : null}
             </Wrapper>
 
-            <img src={nishika} alt="Nishika Patel, the internal vice president of CURIS" className="smallImg"/>
+            <div className = "imgCont"><img src={nishika} width="250px" alt="Nishika Patel, the internal vice president of CURIS" className="smallImg"/></div>
             <h4>Nishika Patel</h4>
             <h5>Internal VP</h5>
           </div>
 
-          <div className="imgAndName">
-            <img src={geeta} alt="Geeta Rastogi, the external vice president of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={geeta} width="250px" alt="Geeta Rastogi, the external vice president of CURIS" /></div>
             <h4>Geeta Rastogi</h4>
             <h5>External VP</h5>
           </div>
-          <div className="imgAndName">
-            <img src={claire} alt="Claire Gleason, the social chair of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={claire} width="250px" alt="Claire Gleason, the social chair of CURIS" /></div>
             <h4>Claire Gleason</h4>
             <h5>Social Chair</h5>
           </div>
-          <div className="imgAndName">
-            <img src={srijani} alt="Srijani Sengupta, the secretary of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={srijani} width="320px" alt="Srijani Sengupta, the secretary of CURIS" /></div>
             <h4>Srijani Sengupta</h4>
             <h5>Secretary</h5>
           </div>
-          <div className="imgAndName">
-            <img src={emma} alt="Emma Suh, the treasurer of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={emma} width="290px" alt="Emma Suh, the treasurer of CURIS" /></div>
             <h4>Emma Suh</h4>
             <h5>Treasurer</h5>
           </div>
@@ -122,43 +122,43 @@ const Members = ({children, src, alt, Wrapper = 'div'}) => {
 
         <h1>Meet Our Committee Leads</h1>
         <div className="grid">
-          <div className="imgAndName">
-            <img src={samantha} alt="Samantha Gordon, the child care network lead of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={samantha} width="320px" alt="Samantha Gordon, the child care network lead of CURIS" /></div>
             <h4>Samantha Gordon</h4>
             <h5>Child Care Network</h5>
           </div>
-          <div className="imgAndName">
-            <img src={vaishnavi} alt="Vaishnavi Krishnan, a family empowerment program lead of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={vaishnavi} width="320px" alt="Vaishnavi Krishnan, a family empowerment program lead of CURIS" /></div>
             <h4>Vaishnavi Krishnan</h4>
             <h5>Family Empowerment Program</h5>
           </div>
-          <div className="imgAndName">
-            <img src={christina} alt="Christina Schafer, a family empowerment program lead of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={christina} width="320px" alt="Christina Schafer, a family empowerment program lead of CURIS" /></div>
             <h4>Christina Schafer</h4>
             <h5>Family Empowerment Program</h5>
           </div>
-          <div className="imgAndName">
-            <img src={aditi} alt="Aditi Vijendra, the fresh start clubhouse lead of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={aditi} width="320px" alt="Aditi Vijendra, the fresh start clubhouse lead of CURIS" /></div>
             <h4>Aditi Vijendra</h4>
             <h5>Fresh Start Clubhouse</h5>
           </div>
-          <div className="imgAndName">
-            <img src={vivian} alt="Vivian Chen, the Hope clinic lead of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={vivian} width="320px" alt="Vivian Chen, the Hope clinic lead of CURIS" /></div>
             <h4>Vivian Chen</h4>
             <h5>Hope Clinic</h5>
           </div>
-          <div className="imgAndName">
-            <img src={daisey} alt="Daisey Yu, a program for multicultural health lead of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={daisey} width="320px" alt="Daisey Yu, a program for multicultural health lead of CURIS" /></div>
             <h4>Daisey Yu</h4>
             <h5>Program for Multicultural Health</h5>
           </div>
-          <div className="imgAndName">
-            <img src={lakshmi} alt="Lakshmi Meyyappan, a program for multicultural health lead of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={lakshmi} width="320px" alt="Lakshmi Meyyappan, a program for multicultural health lead of CURIS" /></div>
             <h4>Lakshmi Meyyappan</h4>
             <h5>Program for Multicultural Health</h5>
           </div>
-          <div className="imgAndName">
-            <img src={avani} alt="Avani Samandur, the Washtenaw health initiative lead of CURIS" />
+          <div className="imgAndName2">
+            <div className = "imgCont"><img src={avani} width="320px" alt="Avani Samandur, the Washtenaw health initiative lead of CURIS" /></div>
             <h4>Avani Samandur</h4>
             <h5>Washtenaw Health Initiative</h5>
           </div>
