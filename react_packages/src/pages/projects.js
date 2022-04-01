93f76cdc0753bd86372b5eec479ddfcaf0dc5f2e
vinayakk <vinayakk@umich.edu>
2022-03-31 20:02:54 -0400

    Project page work, updated styling

--- react_packages/src/pages/projects.js
+++ react_packages/src/pages/projects.js
@@ -2,7 +2,7 @@ import './projects.css';
 import React, {useState} from 'react';
 //import App from './App';
 import whi from '../assets/whi.png'
-import safehouse from '../assets/safehouse.jpeg'
+import safehouse from '../assets/safehouse.png'
 import washalive from '../assets/washalive.png'
 import bagf from '../assets/BAGfoundation.png'
 import nami from '../assets/nami.png'
@@ -19,7 +19,7 @@ const Projects = () => {
         super(props);
         const projArray = props.projects.map((project) => {
           return(
-            <Project index={project.index} imgUrl={project.imgUrl} description={project.description}></Project>
+              <Project index={project.index} imgUrl={project.imgUrl} description={project.description}></Project>
           );
         });
         this.state = {
@@ -39,9 +39,9 @@ const Projects = () => {
   
     render() {
       const {projArray} = this.state;
-      return (<div>
+      return (<div class='block'>
           <div onClick={(e)=>this.togglePanel(e)} className='header'>
-              <h2>{this.props.yearStart}-{this.props.yearEnd}</h2></div>
+              <h4>{this.props.yearStart}-{this.props.yearEnd}</h4></div>
           {this.state.open ? (
               <div className='content'>
                   {projArray.map((project) => {
@@ -59,13 +59,13 @@ const Projects = () => {
     return(
       (props.index % 2 === 0) ?
       <div class = 'rowC'>
-        <img src={props.imgUrl} alt="" class="left"></img>
+        <img src={props.imgUrl} alt="" class="imgLeft"></img>
         <p class="right">{props.description}</p>
       </div>
       :
       <div class = 'rowC'>
         <p class="left">{props.description}</p>
-        <img src={props.imgUrl} alt="" class="right"></img>
+        <img src={props.imgUrl} alt="" class="imgRight"></img>
       </div>
     );
   }
@@ -77,7 +77,7 @@ const Projects = () => {
       "\tScenario based discussions about causes of homelessness:\n" +
       "\t\tCURIS members led small groups in discussion about a variety of theoretical scenarios that might lead to a person losing their housing.\n" +
       "\tSNAP Challenge activity for food insecurity awareness:\n" +
-      "\t\tParticipants were divided into teams and given the challenge to buy a week’s worth of food to fulfill government nutrition requirements on an average SNAP budget."},
+      "\t\tParticipants were divided into teams and given the challenge to buy a week’s worth of food to colfill government nutrition requirements on an average SNAP budget."},
     {index: 1, imgUrl: foodinsec, description: "Our committee researched the intersection between COVID-19 and growing food insecurity. As the pandemic has affected social determinants of health such as SES and employment, more populations have been experiencing food insecurity. Our article posted on Michigan Daily focuses on what resources are available for those who need help and how we can help as a community."},
     {index: 2, imgUrl: jobsec, description: "The Job Security committee (led by Emily Guo with Nisha, Vennela, Jenna) researched and wrote an article about the challenges that people experiencing homelessness face with finding stable jobs and possible interventions. This article was posted in the School of Public Health Pursuit blog. "},
     {index: 3, imgUrl: housingresearch, description: "Our committee is researched policies related to housing development in Ann Arbor as well as Washtenaw County in order to understand and raise awareness about affordable housing options available to affected individuals and families."}
@@ -86,7 +86,7 @@ const Projects = () => {
   const projectsList2021 = [
     {index: 0, imgUrl: washalive, description: "Partnered with Washtenaw Alive Suicide Coalition, WA Survey committee is dedicated to providing mental health support to UofM students. Because of prevalence of mental health issues among college students and the difficulty of a remote school environment, our committee is creating an interactive mental health resource website for students. We will also be conducting surveys on awareness of resources available, how useful students feel those resources are and how COVID-19 has affected mental health."},
     {index: 1, imgUrl: bagf, description: "We are partnered with The Bethany Anne Galdes Foundation to help them decipher Michigan’s mental health policies as they apply to minors. The Bethany Anne Galdes Foundation has a goal of founding the Bethany House as an alternative to hospitalization for minors struggling with depression. Our research has helped determine what kinds of policy changes are necessary to support both evidence-based, depression-specific inpatient treatment and alternative options to hospitalization in crisis situations."},
-    {index: 2, imgUrl: safehouse, description: "The SafeHouse Center is a non-profit organization that provides support and advocacy services to .CURIS is currently working to help SafeHouse Center spread awareness about their resources and mission through social media and graphic design. We are currently working on graphics and social media campaigns to spread awareness about sexual assault, domestic violence, and LGBTQIA+ resources."},
+    {index: 2, imgUrl: safehouse, description: "The SafeHouse Center is a non-profit organization that provides support and advocacy services to CURIS is currently working to help SafeHouse Center spread awareness about their resources and mission through social media and graphic design. We are currently working on graphics and social media campaigns to spread awareness about sexual assault, domestic violence, and LGBTQIA+ resources."},
     {index: 3, imgUrl: whi, description: "The Washtenaw Health Initiative (WHI) is “a voluntary collaboration between more than 200 individual and organizational stakeholders dedicated to improving the health of low-income, uninsured, and under-insured populations across Washtenaw County, Michigan.” In the summer of 2021, the Washtenaw Health Initiative will celebrate its tenth anniversary. At that event, the WHI would like to highlight major accomplishments and reinvigorate the community around shared objectives. To identify and better understand major accomplishments, as well as the challenges ahead, the committee interviewed key stakeholders, collected important historical documents to be used in the construction of an interactive timeline, and checked back on early projects to learn how they’ve evolved over time and what the impact has been."},
     {index: 4, imgUrl: nami, description: "We partnered with NAMI (Michigan), the National Alliance on Mental Illness, to help them complete a mental health services assessment for the Ypsilanti and Whitmore Lake areas. The mental health services assessment was needed to gain a better understanding of what resources are needed in these areas. The assessment included a survey, focus groups, and interviews with community members. Our committee helped review and edit the survey as well as the questions designed for the focus groups."},
     {index: 5, imgUrl: tme, description: "Track My Exposure was a startup questionnaire to help people calculate their risk of COVID-19 based on their daily habits and share the results with their friends. Committee members wrote blog posts on several COVID-19 related topics like COVID-19 in greek life, study spots, college students’ experiences, and the value of providing a risk score rather than relying on cognitive biases."}
